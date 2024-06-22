3299e2633724ba41817d5a2670b37d5afd76ae74
Koushal Thakur <119859253+KoushalThakur@users.noreply.github.com>
2024-06-22 13:38:58 +0530

    Update Footer.jsx

--- Project/src/components/Footer.jsx
+++ Project/src/components/Footer.jsx
@@ -1,6 +1,5 @@
-// import React from 'react';
+import React from 'react';
 import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
-
 
 const Footer = () => {
   return (
@@ -12,10 +11,18 @@ const Footer = () => {
             Discover the latest trends in footwear. Our brand brings you the best in comfort and style.
           </p>
           <div className="socials">
-            <a href="#"><FaFacebookF /></a>
-            <a href="#"><FaTwitter /></a>
-            <a href="#"><FaInstagram /></a>
-            <a href="#"><FaLinkedinIn /></a>
+            <a href="https://www.facebook.com/yourprofile" target="_blank" rel="noopener noreferrer">
+              <FaFacebookF />
+            </a>
+            <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">
+              <FaTwitter />
+            </a>
+            <a href="https://www.instagram.com/yourprofile" target="_blank" rel="noopener noreferrer">
+              <FaInstagram />
+            </a>
+            <a href="https://www.linkedin.com/in/koushal-thakur-857051231/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedinIn />
+            </a>
           </div>
         </div>
         <div className="footer-section links">
